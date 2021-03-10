e75fcbeb9db6f840299c27c261cc65b2a04e4879
Allen Wu <arashilen@gmail.com>
2021-03-10 03:08:10 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+Based on https://packaging.python.org/tutorials/packaging-projects/
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deep-sort", # Replace with your own username
+    version="0.0.1",
+    author="",
+    author_email="",
+    description="Deep Sort Tracker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/allenwu5/deep_sort",
+    project_urls={
+        "Bug Tracker": "https://github.com/allenwu5/deep_sort",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['deep_sort'],
+    python_requires=">=3.6",
+)
